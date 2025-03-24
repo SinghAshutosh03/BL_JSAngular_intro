144db95143514dc51034ad4649f4ddbfd262f8aa
SinghAshutosh03 <ashutoshsingh030302@gmail.com>
2025-03-24 22:46:48 +0530

    UC 3: Open BridgeLabz website on logo click using Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = "Hello from BridgeLabz";
   imgUrl: string = "https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75";  // Add the image URL
+
+  openWebsite(): void {
+    window.open("https://www.bridgelabz.com", "_blank");  // Opens in a new tab
+  }
+
 }
+
