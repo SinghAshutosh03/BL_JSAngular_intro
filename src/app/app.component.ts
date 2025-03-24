c232af022a0946371652b26c247f6eea8aa33e81
SinghAshutosh03 <ashutoshsingh030302@gmail.com>
2025-03-24 23:00:57 +0530

    UC 4: Centered content (Hello, Image, and Input field)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = "Hello from BridgeLabz";
   imgUrl: string = "https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75";  // Add the image URL
-
+  userName: string = "";
   openWebsite(): void {
     window.open("https://www.bridgelabz.com", "_blank");  // Opens in a new tab
   }
+
+  
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';  // Required for ngModel (UC4)
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],  // Import FormsModule here
   providers: [],
   bootstrap: [AppComponent]
 })
